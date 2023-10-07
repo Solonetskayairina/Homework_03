f4724ec4a8067ad9ecd0b0a8148b8ef8c3a40805
Solonetskayairina <Solonetskayairina@ukr.net>
2023-10-07 20:11:54 +0300

    changed files

--- js/index.js
+++ js/index.js
@@ -1,55 +1,61 @@
-    let operations = ['add', 'sub', 'mult', 'div'];
-    let actions = ['+', '-', '*', '/'];
-    let operation = prompt('Select an operation: add, sub, mult, div');
+    const operations = ['add', 'sub', 'mult', 'div'];
+    let operation;
+    let number1;
+    let number2;
+    let result;
 
-    if ( !isOperationValid() ) {
-        alert('There is no such operation')
-        operation = prompt('Try again')
-    }
-        let num1 = prompt('Enter the first number');
-            if ( isNaN(num1) ) {
-                alert('You can only enter numbers')
-                num1 = prompt('Try again')
-            }
 
-        let num2 = prompt('Enter the second number');
-            if ( isNaN(num2) ) {
-                alert('You can only enter numbers')
-                num2 = prompt('Try again')
-            }
+    do {
+      operation = prompt('Select an operation: add, sub, mult, div');
+    } while ( !isOperationValid() );
 
-        num1 = Number(num1)
-        num2 = Number(num2)
+    do {
+        number1 = prompt('Enter the first number');
+    } while ( isNaN(number1) );
 
-        let result;
-        let action;
+    do {
+        number2 = prompt('Enter the second number');
+    } while ( isNaN(number2) );
+
+        const convertedData1 = Number(number1)
+        const convertedData2 = Number(number2)
+
+
+        const getActionFromOperation = (operation) => {
+        const mathSymbols = {
+            add: '+',
+            sub: '-',
+            mult: '*',
+            div: '/'
+        }
+        return mathSymbols[operation]
+        }
 
         if (operation === 'add') {
-            result = num1 + num2;
-            action = actions[0]
+            result = convertedData1 + convertedData2;
+
         }
         if (operation === 'sub') {
-            result = num1 - num2;
-            action = actions[1]
+            result = convertedData1 - convertedData2;
+
         }
         if (operation === 'mult') {
-            result = num1 * num2;
-            action = actions[2]
+            result = convertedData1 * convertedData2;
+
         }
         if (operation === 'div') {
-            if (num2 !== 0) {
-                result = num1 / num2;
-                action = actions[3]
+            if (convertedData2 !== 0) {
+                result = convertedData1 / convertedData2;
+
             } else {
                 alert('Division by zero is not possible');
             }
         }
         if (result) {
-            alert(num1 + ' ' + action + ' ' + num2 + ' = ' + result);
-        } else {
-            alert('You have provided incorrect data, please reload the page')
+            alert(`${number1} ${getActionFromOperation(operation)} ${number2} = ${result}`);
         }
 
     function isOperationValid() {
         return !operation || operations.includes( operation.toLowerCase() )
     }
+
